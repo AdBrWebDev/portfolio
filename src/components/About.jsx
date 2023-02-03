f2c683fbd6958b6576a46200b700f7ff8af68763
ADwebdev <broskoadam@gmail.com>
2023-02-03 22:52:55 +0100

    about section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,36 @@
-import {skillsImgs} from '../data'
-
 export default function About(){
     return(
         <>
             <section class="hero is-medium is-info">
                 <div class="hero-body">
-                <h1 className="text-center text-5xl">About me</h1>
+                <h1 className="text-center text-6xl mb-10">About me</h1>
                 <div className="grid xl:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-                    <div>
-                    <img src="" alt="" />
+                    <div className="text-center">
+                    <h2 className="text-4xl mb-5">Who am I?</h2>
+                    <p className="text-center mx-auto" style={{width: "50%"}}>I'm Fullstack developer in Slovakia. I love new things, I have serious passion for design and dynamic user experiences.</p>
                     </div>
-                    <div>
+                    <div className="text-center">
+                        <h2 className="text-4xl mb-5">What I do?</h2>
                         <ul>
-                            <li>web sites</li>
+                            <li>web sites/apps</li>
                             <li>mobile apps</li>
-                            <li>desktop/web apps</li>
-                            <li>iot</li>
+                            <li>desktop apps</li>
+                            <li>iot(internet of things)</li>
                             <li>web servers</li>
                             <li>modeling</li>
                             <li>3d printing</li>
                         </ul>
                     </div>
                 </div>
+                <h1 className="text-4xl mb-5 text-center mt-20">My strengths</h1>
+                <div className="grid xl:grid-cols-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 text-center">
+                    <div>Precise</div>
+                    <div>Fast learning</div>
+                    <div>Fast</div>
+                    <div>Responsive</div>
+                </div>
                 </div>
             </section>
-            <div className="grid xl:grid-cols-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
-                {skillsImgs.map((imgs, index) => <>
-                
-                </>)}
-            </div>
         </>
     )
 }
--- src/data.js
+++ src/data.js
@@ -1,20 +1,20 @@
 export const frontend = [
-    {progress: '92', label: 'html5', icon: 'devicon-html5-plain'},
-    {progress: '85', label: 'css3', icon: 'devicon-css3-plain'},
+    {progress: '100', label: 'html5', icon: 'devicon-html5-plain'},
+    {progress: '90', label: 'css3', icon: 'devicon-css3-plain'},
     {progress: '80', label: 'javascript', icon: 'devicon-javascript-plain'},
     {progress: '95', label: 'git', icon: 'devicon-git-plain'},
-    {progress: '85', label: 'github', icon: 'devicon-github-original'},
-    {progress: '91', label: 'npm', icon: 'devicon-npm-original-wordmark'},
-    {progress: '80', label: 'yarn', icon: 'devicon-yarn-plain'},
+    {progress: '90', label: 'github', icon: 'devicon-github-original'},
+    {progress: '95', label: 'npm', icon: 'devicon-npm-original-wordmark'},
+    {progress: '90', label: 'yarn', icon: 'devicon-yarn-plain'},
     {progress: '85', label: 'React', icon: 'devicon-react-original'},
     {progress: '65', label: 'Vue', icon: 'devicon-vuejs-plain'},
-    {progress: '80', label: 'bootstrap', icon: 'devicon-bootstrap-plain'},
+    {progress: '85', label: 'bootstrap', icon: 'devicon-bootstrap-plain'},
     {progress: '85', label: 'material-ui', icon: 'devicon-materialui-plain'},
     {progress: '85', label: 'chakra-ui', icon: ''},
-    {progress: '70', label: 'mantine', icon: ''},
+    {progress: '60', label: 'mantine', icon: ''},
     {progress: '65', label: 'tailwind-css', icon: 'devicon-tailwindcss-original-wordmark'},
-    {progress: '80', label: 'bulma', icon: 'devicon-bulma-plain'},
-    {progress: '75', label: 'typecript', icon: 'devicon-typescript-plain'},
+    {progress: '85', label: 'bulma', icon: 'devicon-bulma-plain'},
+    {progress: '70', label: 'typecript', icon: 'devicon-typescript-plain'},
     {progress: '70', label: 'redux', icon: 'devicon-redux-original'},
     {progress: '50', label: 'pinia', icon: ''},
     {progress: '80', label: 'react-native', icon: 'devicon-react-original'},
@@ -26,19 +26,19 @@ export const frontend = [
 export const backend = [
     {progress: '70', label: 'node-js', icon: 'devicon-nodejs-plain'},
     {progress: '49', label: 'python', icon: 'devicon-python-plain'},
-    {progress: '85', label: 'php', icon: 'devicon-php-plain'},
+    {progress: '80', label: 'php', icon: 'devicon-php-plain'},
     {progress: '90', label: 'mysql', icon: 'devicon-mysql-plain'},
     {progress: '50', label: 'mongo', icon: 'devicon-mongodb-plain'},
     {progress: '50', label: 'firebase', icon: 'devicon-firebase-plain'},
     {progress: '55', label: 'cassandra', icon: ''},
-    {progress: '30', label: 'infuxdb', icon: ''},
+    {progress: '25', label: 'infuxdb', icon: ''},
     {progress: '60', label: 'kubernetes', icon: 'devicon-kubernetes-plain'},
     {progress: '60', label: 'docker', icon: 'devicon-docker-plain'},
     {progress: '75', label: 'express.js', icon: 'devicon-express-original'},
 ]
 
 export const servers = [
-    {progress: '40', label: 'nginx', icon: 'devicon-nginx-original'},
+    {progress: '45', label: 'nginx', icon: 'devicon-nginx-original'},
     {progress: '65', label: 'apache', icon: 'devicon-apache-plain'},
 ]
 
@@ -55,13 +55,4 @@ export const os = [
 
 export const microcontrollers = [
     {progress: '80', label: 'raspberry pi', icon: 'devicon-raspberrypi-line'},
-]
-
-export const skillsImgs = [
-    {img: 'web.jpg', span: '2'},
-    {img: 'app.jpg', span: '1'},
-    {img: 'iot.jpg', span: '1'},
-    {img: '3dprinting.jpg', span: '2'},
-    {img: 'webservers.jpg', span: '1'},
-    {img: 'modeling.jpg', span: '2'},
 ]
