d2200560a7443e8e2ebab18f1611e9dc70d9d7b2
ADwebdev <broskoadam@gmail.com>
2023-02-03 08:41:43 +0100

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
-
-
 export default function Footer(){
 
     return(
         <>
-        
+            <div className="container mx-auto text-center pb-5 my-5 divide-y">
+                    
+                        <div>
+                            <h1 className="mb-2">Follow me</h1>
+                        </div>
+                        <a href="https://www.linkedin.com/in/adam-brosko-a90785243/"><button type="button" className="shadow-inner"><i className="fa fa-facebook" style={{"transform": "scale(2)", color: "#1aa3ff", fontWeight: "bold"}}></i></button></a>
+                    
+            </div>
         </>
     )
 }
