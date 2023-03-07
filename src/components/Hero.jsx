b00bbc49b11aacfac4476fd677616e9ec3f2a73d
ADwebdev <broskoadam@gmail.com>
2023-03-07 20:49:52 +0100

    shadow for hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero(){
 
     return(
         <>
-            <div style={{zIndex: -10, background: "rgba(1,1,1,.4)"}} >
+            <div style={{zIndex: -10, boxShadow: '0 0 200px 40px rgba(1,1,1,.4)'}} >
                 <img src="images/hero.jpg" style={{zIndex: -10, backgroundSize: "cover"}} height={`${window.innerHeight}px`} width={`${window.innerWidth}px`} alt="" />
             </div>
         </>
